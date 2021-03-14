fae0ce4f5b64813d56a3ccfea6871c5b3076abd7
Md. Ibrahim Shaikh <ibrahim.cseku@gmail.com>
2021-03-14 13:50:28 +0600

    Updating the pom.xml and also the document file

--- src/main/resources/db/dbscript.sql
+++ src/main/resources/db/dbscript.sql
@@ -19,5 +19,4 @@ CREATE TABLE roles (
 );
 
 INSERT INTO roles(id,name) VALUES(1,'ROLE_USER');
-INSERT INTO roles(id,name) VALUES(2,'ROLE_MODERATOR');
-INSERT INTO roles(id,name) VALUES(3,'ROLE_ADMIN');
+INSERT INTO roles(id,name) VALUES(2,'ROLE_ADMIN');
